4f7224c8cbab8f32c20f5ec5cfc79afea045f347
salypimientado <juanpablosalazar63@hotmail.com>
2021-03-18 12:43:07 -0700

    variables automata

--- school/sem4/implementation-of-computational-methods/hw-projects/Act3-2-Lexer/lexer.hs
+++ school/sem4/implementation-of-computational-methods/hw-projects/Act3-2-Lexer/lexer.hs
@@ -5,4 +5,24 @@ main = do
   hFlush stdout
   file <- getLine
   contents <- readFile file
-  putStr contents
+  lexed <- return $ lexer (lines contents)
+  putStr $ unlines $ concat $ map (\line -> map show line) lexed
+
+lexer :: [[Char]] -> [[( Char, Int )]]
+lexer lines = map (\line -> map (flip (lexChar) 0) line) lines 
+
+lexChar :: Char -> Int -> (Char, Int)
+lexChar char state
+  | char `elem` ['a'..'z'] = ( char, stateList !! 0 )
+  | char `elem` ['A'..'Z'] = ( char, stateList !! 1 )
+  | char == '_'            = ( char, stateList !! 2 )
+  | char `elem` ['0'..'9'] = ( char, stateList !! 3 )
+  | otherwise              = ( char, last stateList )
+  where e = 999
+        stateMatrix = [
+         {-  [a..z] ~ [A..Z]  ~ _  ~  [0..9]  ~  else-}
+   {-0-} [     1   ,   1  ,     999  , e   ,     e  ],
+   {-1-} [     1   ,   1  ,     1    , 1   ,     101]
+   {-3-}
+                ]
+        stateList = ( stateMatrix !! state )
