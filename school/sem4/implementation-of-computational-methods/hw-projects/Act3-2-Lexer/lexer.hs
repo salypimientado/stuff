50508c57d0cc7b6a30390593164dec7683a27556
salypimientado <juanpablosalazar63@hotmail.com>
2021-03-20 12:06:29 -0700

    act3-2 haskell nextChar done

--- school/sem4/implementation-of-computational-methods/hw-projects/Act3-2-Lexer/lexer.hs
+++ school/sem4/implementation-of-computational-methods/hw-projects/Act3-2-Lexer/lexer.hs
@@ -5,24 +5,42 @@ main = do
   hFlush stdout
   file <- getLine
   contents <- readFile file
-  lexed <- return $ lexer (lines contents)
-  putStr $ unlines $ concat $ map (\line -> map show line) lexed
+  let lexed = lexer (lines contents)
+  putStr $ unlines $ concat $ map (map show) lexed
 
 lexer :: [[Char]] -> [[( Char, Int )]]
-lexer lines = map (\line -> map (flip (lexChar) 0) line) lines 
+lexer = map (map (flip (lexChar) 0))
 
 lexChar :: Char -> Int -> (Char, Int)
 lexChar char state
-  | char `elem` ['a'..'z'] = ( char, stateList !! 0 )
-  | char `elem` ['A'..'Z'] = ( char, stateList !! 1 )
-  | char == '_'            = ( char, stateList !! 2 )
-  | char `elem` ['0'..'9'] = ( char, stateList !! 3 )
+  | char == ' '            = ( char, head stateList )
+  | char `elem` ['a'..'z'] = ( char, stateList !! 1 )
+  | char `elem` ['A'..'Z'] = ( char, stateList !! 2 )
+  | char == '_'            = ( char, stateList !! 3 )
+  | char `elem` ['0'..'9'] = ( char, stateList !! 4 )
+  | char == '='            = ( char, stateList !! 5 )
+  | char == '+'            = ( char, stateList !! 6 )
+  | char == '-'            = ( char, stateList !! 7 )
+  | char == '/'            = ( char, stateList !! 8 )
+  | char == '*'            = ( char, stateList !! 9 )
+  | char == '^'            = ( char, stateList !! 10)
+  | char == '('            = ( char, stateList !! 11)
+  | char == ')'            = ( char, stateList !! 12)
   | otherwise              = ( char, last stateList )
   where e = 999
         stateMatrix = [
-         {-  [a..z] ~ [A..Z]  ~ _  ~  [0..9]  ~  else-}
-   {-0-} [     1   ,   1  ,     999  , e   ,     e  ],
-   {-1-} [     1   ,   1  ,     1    , 1   ,     101]
-   {-3-}
-                ]
-        stateList = ( stateMatrix !! state )
+         {- space     [a..z]   [A..Z]    _     [0..9]     =    +     -     /     *    ^   (     )     else-}
+   {- 0 -} [   0,       1,       1,      e,      e,       2,   3,    4,    5,    7,   8,  9,    10,   e    ],
+   {- 1 -} [   101,     1,       1,      1,      1] ++ repeat 101,
+   {- 2 -} repeat 102,
+   {- 3 -} repeat 103,
+   {- 4 -} repeat 104,
+   {- 5 -} replicate 8 105  ++ [6] ++ repeat 105,
+   {- 6 -} repeat 6,
+   {- 7 -} repeat 107,
+   {- 8 -} repeat 108,
+   {- 9 -} repeat 109,
+   {- 10-} repeat 110
+
+         ]
+        stateList = stateMatrix !! state
